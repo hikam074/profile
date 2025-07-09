8b74cce224f5a4e05156bd59cad8c1070f2fedd8
hikam074 <hikam074@gmail.com>
2025-07-09 22:13:54 +0700

    fixed: typo

--- src/data/pengalamans.jsx
+++ src/data/pengalamans.jsx
@@ -2,7 +2,7 @@ import { MdWork, MdSchool } from "react-icons/md";
 
 const pengalaman = [
     {
-      title: "Asisten Praktikum",
+      title: "Asisten Praktikum Sistem Basis Data",
       subtitle: "Laboratorium Rekayasa Perangkat Lunak UNEJ",
       date: "Februari 2025 - juni 2025",
       deskripsi: "Mengajar 40+ mahasiswa mata kuliah Sistem Basis Data dengan pendekatan praktis, menyediakan modul dan asistensi, serta mengelola evaluasi hingga capaian nilai rata-rata 88,58.",
@@ -10,7 +10,7 @@ const pengalaman = [
       backgroundColor: "#2196f3"
     },
     {
-      title: "Penanggungjawab TIm Hadrah Fakultas Ilmu Komputer UNEJ",
+      title: "Penanggungjawab Tim Hadrah Fakultas Ilmu Komputer UNEJ",
       subtitle: "UKM-KI Al-Azhar",
       date: "Februari 2024 - ongoing",
       deskripsi: "Mengkoordinasi 15+ anggota tim hadrah dengan strategi berbasis potensi individu hingga meraih Juara 1 Hadrah Festival Mustika UNEJ 2024.",
